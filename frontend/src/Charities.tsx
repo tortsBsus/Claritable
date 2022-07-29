7e474a04dd6bafbebc82ebd0db5f19b8ccc236f0
kittycodes <77451065+lekhanakraj@users.noreply.github.com>
2022-07-30 01:30:19 +0530

    charity input numbers

--- frontend/src/Charities.tsx
+++ frontend/src/Charities.tsx
@@ -2,10 +2,13 @@ import { FunctionComponent } from "react";
 import { Button } from "react-bootstrap";
 import ReactDOM from "react-dom";
 import styles from "./css/Charities.module.css";
-import Popup from "reactjs-popup";
+import { useState } from 'react';
+
 
 export const Charities: FunctionComponent = () => {
-  
+  const [num, setNum] = useState("");
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");//number 
   return (
     <div className={styles.charitiesDiv}>
       <div className={styles.frameDiv}>
@@ -23,8 +26,13 @@ export const Charities: FunctionComponent = () => {
       </div>
       <div className={styles.cARD1Div}>
         <div className={styles.frameDiv1}>
-          <div className={styles.rectangleDiv} />
-          <Popup modal trigger={<Button className={styles.rAISENOWDiv}>RAISE NOW</Button>}>Raise</Popup>
+          <div className={styles.rectangleDiv} ><input type="number"  min="1" 
+          value={num}
+          onChange={(e) => setNum(e.target.value)} />
+          <div>You are willing to donate :{num}</div></div>
+          
+          <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
+          
           <div className={styles.rectangleDiv1} />
         </div>
         <div className={styles.eDUCATIONDiv}>EDUCATION</div>
@@ -32,8 +40,12 @@ export const Charities: FunctionComponent = () => {
       <div className={styles.cARD2Div}>
         <div className={styles.frameDiv2}>
           <div className={styles.frameDiv3}>
-            <div className={styles.rectangleDiv} />
+           
             <div className={styles.rectangleDiv1} />
+            <div className={styles.rectangleDiv} ><input type="number"  min="1" 
+          value={num1}
+          onChange={(e) => setNum1(e.target.value)} />
+          <div>You are willing to donate :{num1}</div></div>
             <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
           </div>
           <div className={styles.gIRLCHILDEMPOWERMENT}>
@@ -44,7 +56,11 @@ export const Charities: FunctionComponent = () => {
       <div className={styles.cARD3Div}>
         <div className={styles.frameDiv2}>
           <div className={styles.frameDiv3}>
-            <div className={styles.rectangleDiv} />
+            
+            <div className={styles.rectangleDiv} ><input type="number" min="1" 
+          value={num2}
+          onChange={(e) => setNum2(e.target.value)} />
+          <div>You are willing to donate :{num2}</div></div>
             <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
             <div className={styles.rectangleDiv1} />
           </div>
