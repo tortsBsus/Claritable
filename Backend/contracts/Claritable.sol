d9fab4e11aac1dde18cbab8f3f764406ce6960b8
tortsBsus <meganraj251@gmail.com>
2022-07-30 09:36:49 +0530

    presentation commit

--- Backend/contracts/Claritable.sol
+++ Backend/contracts/Claritable.sol
@@ -5,7 +5,61 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Claritable {
-    constructor() {
-        console.log("Smart Contract created");
+
+    uint256 private seed;
+
+    address payable GirlCharityAddress; 
+    address payable UkraineCharityAddress;
+    address payable EducationCharityAddress;
+
+    struct GirlChildSponsors{
+        address who_address;
+        uint don_amount;
     }
+
+    struct UkraineSponsors{
+        
+        address who_address;
+        uint don_amount;
+    }
+
+    struct EducationSponsors{
+        
+        address who_address;
+        uint don_amount;
+    }
+    
+    
+    EducationSponsors [] spon1;
+    GirlChildSponsors [] spon2;
+    UkraineSponsors [] spon3;
+    
+
+    constructor() payable{
+        console.log("Smart Contract created");
+        GirlCharityAddress = payable(0xDadAE8A1A200Fd9D6797E6b0B96ee35f40ad5ed5);
+        UkraineCharityAddress = payable(0xd0750ace4F221dEa3B3523508a3Cb83C474d13c5);//account 1
+        EducationCharityAddress = payable(0x1892D4A3cCE85DD4eaa269B052eFbFebd45BeC43);//account2
+
+    }
+
+    function donate(uint charID, uint amt, address add) payable public
+    {
+        if(charID == 0){
+            console.log("You have sponsored Education");
+            EducationCharityAddress.transfer(msg.value);
+        }
+        else if(charID == 1){
+            console.log("You have sponsored Girl Empowerment");
+            GirlCharityAddress.transfer(msg.value);
+        }
+        else {
+            console.log("You have sponsored Ukraine");
+            UkraineCharityAddress.transfer(msg.value);
+        }
+        console.log("Your donation is registered");
+               
+    }
+
+   
 }
--- frontend/src/Charities.tsx
+++ frontend/src/Charities.tsx
@@ -2,17 +2,60 @@ import { FunctionComponent } from "react";
 import { Button } from "react-bootstrap";
 import ReactDOM from "react-dom";
 import styles from "./css/Charities.module.css";
-import { useState } from 'react';
-
+import { useState } from "react";
+import abi from "./utils/abi.json";
+import { ethers }  from "ethers";
+declare let window: any;
 
 export const Charities: FunctionComponent = () => {
   const [num, setNum] = useState("");
   const [num1, setNum1] = useState("");
-  const [num2, setNum2] = useState("");//number 
+  const [num2, setNum2] = useState(""); //number
+  const contractAddress = "0x7013C014bDec413cD873e97740F58E2Ba821Af70";
+  const contractABI = abi.abi;
+
+
+  //function 3
+  const wave = async () => {
+  
+    console.log("entered wave function");
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const alienContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        try {
+          const Txn = await alienContract.faceInc("message",{ gasLimit: 300000 });
+
+          console.log("Mining...", Txn.hash);
+
+          await Txn.wait();
+          console.log("Mined and paid! ", Txn.hash);
+      }
+      catch (error) {
+          alert("Wait 2 mins between votes! Refresh page and try again!");
+          return;
+      }
+
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("the error? " + error);
+    }
+  };
+
   return (
     <div className={styles.charitiesDiv}>
       <div className={styles.frameDiv}>
-      
         <div className={styles.cLARITABLEDiv}>
           <span className={styles.cLARITABLSpan}>CLARITABL</span>
           <span className={styles.eSpan}>E</span>
@@ -26,13 +69,18 @@ export const Charities: FunctionComponent = () => {
       </div>
       <div className={styles.cARD1Div}>
         <div className={styles.frameDiv1}>
-          <div className={styles.rectangleDiv} ><input type="number"  min="1" 
-          value={num}
-          onChange={(e) => setNum(e.target.value)} />
-          <div>You are willing to donate :{num}</div></div>
-          
-          <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
-          
+          <div className={styles.rectangleDiv}>
+            <input
+              type="number"
+              min="1"
+              value={num}
+              onChange={(e) => setNum(e.target.value)}
+            />
+            <div>You are willing to donate :{num}</div>
+          </div>
+
+          <Button className={styles.rAISENOWDiv} onClick = {wave}>RAISE NOW</Button>
+
           <div className={styles.rectangleDiv1} />
         </div>
         <div className={styles.eDUCATIONDiv}>EDUCATION</div>
@@ -40,13 +88,17 @@ export const Charities: FunctionComponent = () => {
       <div className={styles.cARD2Div}>
         <div className={styles.frameDiv2}>
           <div className={styles.frameDiv3}>
-           
             <div className={styles.rectangleDiv1} />
-            <div className={styles.rectangleDiv} ><input type="number"  min="1" 
-          value={num1}
-          onChange={(e) => setNum1(e.target.value)} />
-          <div>You are willing to donate :{num1}</div></div>
-            <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
+            <div className={styles.rectangleDiv}>
+              <input
+                type="number"
+                min="1"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+              />
+              <div>You are willing to donate :{num1}</div>
+            </div>
+            <Button className={styles.rAISENOWDiv} onClick = {wave}>RAISE NOW</Button>
           </div>
           <div className={styles.gIRLCHILDEMPOWERMENT}>
             GIRL CHILD EMPOWERMENT
@@ -56,12 +108,16 @@ export const Charities: FunctionComponent = () => {
       <div className={styles.cARD3Div}>
         <div className={styles.frameDiv2}>
           <div className={styles.frameDiv3}>
-            
-            <div className={styles.rectangleDiv} ><input type="number" min="1" 
-          value={num2}
-          onChange={(e) => setNum2(e.target.value)} />
-          <div>You are willing to donate :{num2}</div></div>
-            <Button className={styles.rAISENOWDiv}>RAISE NOW</Button>
+            <div className={styles.rectangleDiv}>
+              <input
+                type="number"
+                min="1"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+              />
+              <div>You are willing to donate :{num2}</div>
+            </div>
+            <Button className={styles.rAISENOWDiv} onClick = {wave}>RAISE NOW</Button>
             <div className={styles.rectangleDiv1} />
           </div>
           <div className={styles.sENDHELPTOUKRAINE}>SEND HELP TO UKRAINE</div>
